f1db50aa16f56c05c703c97cfc15d76fa45929ee
Ky-Anh Huynh <kyanh@theslinux.org>
2014-09-03 08:28:20 +0700

    core_ext/io.rb: use File::open instead of IO::open
    (IO::open) accepts an Integer (a #fd) as its first argument.
    If we have a file name, we need to use (File::open) instead.

--- lib/bencode/core_ext/io.rb
+++ lib/bencode/core_ext/io.rb
@@ -1,10 +1,10 @@
-class IO  
+class IO
   def self.bdecode(filename)
-    open(filename, 'rb') {|io| io.bdecode}
+    File::open(filename, 'rb') {|io| io.bdecode}
   end
 
   def self.bencode(filename)
-    open(filename, 'rb') {|io| io.bencode}
+    File::open(filename, 'rb') {|io| io.bencode}
   end
 
   def bdecode
